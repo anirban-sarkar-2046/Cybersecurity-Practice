53fc87136f37d0c5212e5cc9dc0286e1e4a803c2
Anirban Sarkar <147172432+anirban-sarkar-2046@users.noreply.github.com>
2024-09-25 01:06:17 +0600

    Create information.sh

---
+++ CTF Practices/picoCTF/information.sh
@@ -0,0 +1,51 @@
+anirban@LAPTOP-LL4QBUQF:~$ file cat.jpg
+cat.jpg: JPEG image data, JFIF standard 1.02, aspect ratio, density 1x1, segment length 16, baseline, precision 8, 2560x1598, components 3
+anirban@LAPTOP-LL4QBUQF:~$ strings cat.jpg | head
+JFIF
+0Photoshop 3.0
+8BIM
+PicoCTF
+http://ns.adobe.com/xap/1.0/
+<?xpacket begin='
+' id='W5M0MpCehiHzreSzNTczkc9d'?>
+<x:xmpmeta xmlns:x='adobe:ns:meta/' x:xmptk='Image::ExifTool 10.80'>
+<rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>
+ <rdf:Description rdf:about=''
+anirban@LAPTOP-LL4QBUQF:~$ binwalk cat.jpg
+
+DECIMAL       HEXADECIMAL     DESCRIPTION
+--------------------------------------------------------------------------------
+0             0x0             JPEG image data, JFIF standard 1.02
+
+anirban@LAPTOP-LL4QBUQF:~$ exiftool cat.jpg
+ExifTool Version Number         : 12.40
+File Name                       : cat.jpg
+Directory                       : .
+File Size                       : 858 KiB
+File Modification Date/Time     : 2024:09:15 15:52:42+06:00
+File Access Date/Time           : 2024:09:25 00:57:55+06:00
+File Inode Change Date/Time     : 2024:09:15 16:27:55+06:00
+File Permissions                : -rw-r--r--
+File Type                       : JPEG
+File Type Extension             : jpg
+MIME Type                       : image/jpeg
+JFIF Version                    : 1.02
+Resolution Unit                 : None
+X Resolution                    : 1
+Y Resolution                    : 1
+Current IPTC Digest             : 7a78f3d9cfb1ce42ab5a3aa30573d617
+Copyright Notice                : PicoCTF
+Application Record Version      : 4
+XMP Toolkit                     : Image::ExifTool 10.80
+License                         : cGljb0NURnt0aGVfbTN0YWRhdGFfMXNfbW9kaWZpZWR9
+Rights                          : PicoCTF
+Image Width                     : 2560
+Image Height                    : 1598
+Encoding Process                : Baseline DCT, Huffman coding
+Bits Per Sample                 : 8
+Color Components                : 3
+Y Cb Cr Sub Sampling            : YCbCr4:2:0 (2 2)
+Image Size                      : 2560x1598
+Megapixels                      : 4.1
+anirban@LAPTOP-LL4QBUQF:~$ echo cGljb0NURnt0aGVfbTN0YWRhdGFfMXNfbW9kaWZpZWR9 | base64 -d
+picoCTF{the_m3tadata_1s_modified}
